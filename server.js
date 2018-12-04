5ba5b5d20caf165d00273312207e3f8b6d66f992
mac <mac@macs-MBP.home>
2018-12-04 02:36:43 +0000

    ok

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 var bodyParser = require('body-parser')
 const mysql = require('mysql');
+var cookieParser = require('cookie-parser')
+var session = require('express-session')
+var passport = require('passport');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
 
 const app = express();
 
-
+//sql create connection
 var db = mysql.createConnection({
     //properties
     host: 'localhost',
-    port:3310,
+    port: 3310,
     user: 'root',
     password: '',
     database: 'thales2018',
-    socketPath:"/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
+    socketPath: "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
 });
 
 db.connect(function (err) {
@@ -23,7 +29,42 @@ db.connect(function (err) {
     }
 });
 
+app.listen(3000, () => console.log('Example applistening on port 3000!!!'));
+
+
+app.use(cookieParser());
 app.use(express.static('client'));
+
+app.use(session({
+    secret: 'jibirish',
+    resave: false,
+    saveUninitialized: false,
+    //cookie: { secure: true }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
+
+app.get('/getUser', (req, res) => {
+    let sql = 'SELECT * FROM user';
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+
+    });
+});
+
 
 app.get('/test', function (req, res) {
     //about my sql
@@ -37,5 +78,3 @@ app.get('/test', function (req, res) {
     });
 })
 
-
-app.listen(3000, () => console.log('Example applistening on port 3000!!!'));
