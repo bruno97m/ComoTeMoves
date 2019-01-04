9382adb1c4719f735e2e8bc4da36fc672444d0f1
mac <mac@macs-MacBook-Pro.local>
2019-01-04 18:20:27 +0000

    login??????

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var bodyParser = require('body-parser')
 var mysql = require('mysql');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 var expressValidator = require('express-validator');
 var passport = require('passport');
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
-var flash = require('connect-flash');
 var http = require('http')
 var app = express();
 var bcrypt = require('bcrypt');
@@ -26,6 +26,20 @@ app.listen(3000, function () {
 
 app.use(cookieParser());
 app.use(express.static('client'));
+
+//express mysql session
+const options = {
+    host: 'localhost',
+    port: 3310,
+    user: 'root',
+    password: '',
+    database: 'thales2018',
+    socketPath: "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
+};
+
+var sessionStore = new MySQLStore(options);
+
+
 app.use(expressValidator({
     errorFormatter: function (param, msg, value) {
         var namespace = param.split('.'),
@@ -44,10 +58,12 @@ app.use(expressValidator({
 }));
 app.use(session({
     secret: 'jibirish',
+    store: sessionStore,
     resave: false,
     saveUninitialized: false,
     //cookie: { secure: true }
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -80,6 +96,14 @@ app.get('/getCoordenadas', function (req, res) {
     });
 });
 
+app.get('/getEstatisticas1', function (req, res) {
+    let sql = "SELECT concat(11*floor(sd.AGE/11), '-', 11*floor(sd.AGE/11) + 10) as 'Age', COUNT(*) AS 'Number' FROM simulation_data AS sd INNER JOIN im_sim_0_pt_checks AS im ON sd.USER_ID = im.USER_ID group by 1 ORDER BY sd.AGE;";
+    console.log(sql);
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
 app.get('/getEstatisticas2', function (req, res) {
     let sql = "SELECT DISTINCT 'BUS' AS Transport, COUNT(*) AS Number FROM im_sim_0_pt_checks AS Number WHERE CHECK_TYPE LIKE '%BUS%' UNION SELECT DISTINCT 'TRAIN' AS Transport, COUNT(*) AS Number FROM im_sim_0_pt_checks AS Number WHERE CHECK_TYPE LIKE '%TRAIN%' UNION SELECT DISTINCT 'SUBWAY' AS Transport, COUNT(*) AS Number FROM im_sim_0_pt_checks AS Number WHERE CHECK_TYPE LIKE '%SUBWAY%';";
@@ -140,9 +164,25 @@ app.post('/submit', function (req, res) {
             // Store hash in your password DB.
             let sql = "INSERT INTO `user` (`id`, `username`, `email`, `password`,`checkpassword`) VALUES (NULL, '" + username + "', '" + email + "', '" + hash + "', '" + hash + "');"
 
-            db.query(sql, [username, email, password, checkpassword], function (err) {
+            db.query(sql, function (err, result) {
                 if (err) throw err;
-                res.sendFile(__dirname + '/client/index.html');
+
+                let sql = "SELECT LAST_INSERT_ID() as user_id";
+
+                db.query(sql, function(err, result){
+                    if (err) throw err;
+
+                    var user_id = result[0];
+                    console.log(result[0]);
+
+                    //LOGIN USER-create a session
+                    req.login(user_id, function (err) {
+
+                        res.redirect('/dashboard.html');
+
+                    });
+
+                });
             });
 
         });
@@ -150,6 +190,80 @@ app.post('/submit', function (req, res) {
 });
 
 
+//Local - for local database strategy
+app.post('/login', passport.authenticate('local', {
 
-// Connect Flash
-app.use(flash());
+    successRedirect: '/dashboard.html',
+    failureRedirect: '/index.html'
+
+}));
+
+app.get('/dashboard', authenticationMiddleware(), function (req, res) {
+
+
+    //  deserializeUser ... if so - creates a session and returns a session key
+    console.log(req.user);
+    console.log(req.isAuthenticated());
+    res.sendFile(__dirname + '/client/dashboard.html');
+
+});
+
+
+//verify if the user exists and the password is correct
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+
+        console.log(username);
+        console.log(password);
+
+
+        db.query('SELECT id, password FROM user WHERE username = ?', [username], function (err, result) {
+            if (err) throw err;
+
+            console.log(result[0]);
+
+            //if nothing is returned
+            if (result.length === 0) {
+                console.log("Empty");
+                done(null, false);
+            }
+
+            const hash = result[0].password.toString();
+            var res = bcrypt.compareSync(password, hash);
+
+            if (res === true) {
+
+                return done(null, {
+                    user_id: result[0].id
+                });
+
+            }
+
+        });
+
+
+
+        return done(null, false);
+
+    }));
+
+
+//writing user data in the session
+passport.serializeUser(function (user_id, done) {
+    done(null, user_id);
+});
+
+//retrieving user datafrom the session
+passport.deserializeUser(function (user_id, done) {
+
+    done(null, user_id);
+});
+
+function authenticationMiddleware() {
+    return function (req, res, next) {
+        console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+        if (req.isAuthenticated()) return next();
+        res.sendFile(__dirname + '/client/index.html');
+    }
+}
