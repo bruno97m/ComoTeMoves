63fc9986bbff50c107492ca34d739348256828d5
mac <mac@macs-MacBook-Pro.local>
2019-01-05 23:45:58 +0000

    Logout

---
+++ routes/login.js
@@ -0,0 +1,83 @@
+var passport = require('passport');
+var passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var db = require('../db');
+
+
+
+
+//Local - for local database strategy
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/dashboard.html',
+    failureRedirect: '/index.html',
+}));
+
+router.get('/logout', function (req,res) {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
+});
+
+
+router.get('/dashboard',authenticationMiddleware (), function (req, res) {
+
+
+    //  deserializeUser ... if so - creates a session and returns a session key
+    console.log(req.user);
+    console.log(req.isAuthenticated());
+    res.redirect('/dashboard.html');
+
+});
+
+//verify if the user exists and the password is correct
+passport.use(new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password'
+    },
+    function (username, password, done) {
+
+        console.log(username);
+        console.log(password);
+
+
+        db.query('SELECT id, password FROM user WHERE username = ?', [username], function (err, result) {
+            if (err) throw err;
+            console.log(err);
+            console.log(result[0]);
+
+            //if nothing is returned
+            if (result.length === 0) {
+                return done(null, false);
+
+            }
+            console.log(result[0].password);
+            console.log(result[0].password.toString());
+            var hash = result[0].password.toString();
+            bcrypt.compare(password, hash, function (err, response) {
+                if (response === true) {
+                    console.log("da");
+                    return done(null, {
+                        user_id: result[0].id
+                    });
+                } else {
+                    console.log("else")
+                    return done(null, false);
+                }
+            });
+        });
+    }
+));
+
+function authenticationMiddleware () {
+  return (req, res, next) => {
+    console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+      if (req.isAuthenticated()) return next();
+      res.redirect('/login')
+  }
+}
+
+module.exports = router;
--- server.js
+++ server.js
@@ -8,21 +8,22 @@ var expressValidator = require('express-validator');
 var passport = require('passport');
 var passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy;
-var http = require('http')
-var app = express();
+var http = require('http');
+var flash = require('connect-flash');
 var bcrypt = require('bcrypt');
-const saltRounds = 10;
+var app = express();
 var router = express.Router();
 var path = require('path');
 var db = require('./db');
+const saltRounds = 10;
+var register = require('./routes/register');
+var login = require('./routes/login');
 
 //sendFile(__dirname+'/public..)
-
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!!!');
 });
-
 
 app.use(cookieParser());
 app.use(express.static('client'));
@@ -36,9 +37,7 @@ const options = {
     database: 'thales2018',
     socketPath: "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
 };
-
 var sessionStore = new MySQLStore(options);
-
 
 app.use(expressValidator({
     errorFormatter: function (param, msg, value) {
@@ -66,28 +65,14 @@ app.use(session({
 
 app.use(passport.initialize());
 app.use(passport.session());
-
-
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: false
-}))
-
-
+}));
+app.use(bodyParser.json());
 // parse application/json
 
-
-app.get('/getUser', function (req, res) {
-    let query = 'SELECT * FROM user';
-    db.query(query, function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        res.send(result);
-
-    });
-});
-
-
+//coordenadas user mapa
 app.get('/getCoordenadas', function (req, res) {
     let sql = 'SELECT ORIGIN_LONGITUDE,ORIGIN_LATITUDE,DESTINATION_LONGITUDE,DESTINATION_LATITUDE,NAME FROM SIMULATION_DATA ORDER BY NAME DESC LIMIT 1';
     db.query(sql, (err, result) => {
@@ -95,7 +80,7 @@ app.get('/getCoordenadas', function (req, res) {
         res.send(result);
     });
 });
-
+   //-----------------------------------------Estatisticas----------------------------------------------------------
 app.get('/getEstatisticas1', function (req, res) {
     let sql = "SELECT concat(11*floor(sd.AGE/11), '-', 11*floor(sd.AGE/11) + 10) as 'Age', COUNT(*) AS 'Number' FROM simulation_data AS sd INNER JOIN im_sim_0_pt_checks AS im ON sd.USER_ID = im.USER_ID group by 1 ORDER BY sd.AGE;";
     console.log(sql);
@@ -104,162 +89,26 @@ app.get('/getEstatisticas1', function (req, res) {
         res.send(result);
     });
 });
-
-app.get('/getEstatisticas2', function (req, res) {
+ app.get('/getEstatisticas2', function (req, res) {
     let sql = "SELECT DISTINCT 'BUS' AS Transport, COUNT(*) AS Number FROM im_sim_0_pt_checks AS Number WHERE CHECK_TYPE LIKE '%BUS%' UNION SELECT DISTINCT 'TRAIN' AS Transport, COUNT(*) AS Number FROM im_sim_0_pt_checks AS Number WHERE CHECK_TYPE LIKE '%TRAIN%' UNION SELECT DISTINCT 'SUBWAY' AS Transport, COUNT(*) AS Number FROM im_sim_0_pt_checks AS Number WHERE CHECK_TYPE LIKE '%SUBWAY%';";
     db.query(sql, (err, result) => {
         if (err) throw err;
         res.send(result);
     });
 });
-
-app.get('/getEstatisticas3', function (req, res) {
+ app.get('/getEstatisticas3', function (req, res) {
     let sql = "SELECT concat(HOUR(CONVERT(DATE_TIME, DATETIME)), '-', HOUR(CONVERT(DATE_TIME, DATETIME)) + 1) as Hour, COUNT(*) AS Number FROM simulation_data group by 1 ORDER BY HOUR(CONVERT(DATE_TIME, DATETIME))";
-    db.query(sql, (err, result) => {
+        db.query(sql, (err, result) => {
         if (err) throw err;
         res.send(result);
     });
 });
 
-app.get('/test', function (req, res) {
-    //about my sql
-    db.query("SELECT * FROM user", function (error, rows, fields) {
-        if (error) {
-            console.log('error in the query');
-        } else {
-            console.log('Successful query');
-            res.send(rows[0].username);
-        }
-    });
+app.get('/logout', function (req,res) {
+    req.logout();
+    req.session.destroy();
+    res.redirect('/');
 });
-
-
-
-
-
-
-
-
-
-
-
-app.post('/submit', function (req, res) {
-    console.log(req.body);
-    var username = req.body.username;
-    var email = req.body.email;
-    var password = req.body.password;
-    var checkpassword = req.body.checkpassword;
-
-    req.checkBody('username', ' Campo obrigatório').notEmpty();
-    req.checkBody('email', ' Campo obrigatório').notEmpty();
-    req.checkBody('password', ' Campo obrigatório').notEmpty();
-    req.checkBody('checkpassword', ' Passwords não coincidem').equals(req.body.password);
-
-
-    var errors = req.validationErrors();
-
-    //console.log(errors);
-
-    if (errors) {
-        //dá erro
-
-        res.writeHead(300, {
-            'Content-Type': 'application/json'
-        });
-        res.write(JSON.stringify(errors));
-        res.send();
-    } else {
-        bcrypt.hash(password, saltRounds, function (err, hash) {
-            // Store hash in your password DB.
-            let sql = "INSERT INTO `user` (`id`, `username`, `email`, `password`,`checkpassword`) VALUES (NULL, '" + username + "', '" + email + "', '" + hash + "', '" + hash + "');"
-
-            db.query(sql, function (err, result) {
-                if (err) throw err;
-
-                let sql = "SELECT LAST_INSERT_ID() as user_id";
-
-                db.query(sql, function (err, result) {
-                    if (err) throw err;
-
-                    var user_id = result[0];
-                    console.log(result[0]);
-
-                    //LOGIN USER-create a session
-                    req.login(user_id, function (err) {
-
-                        res.redirect('/dashboard.html');
-
-                    });
-
-                });
-            });
-
-        });
-    };
-});
-
-
-app.get('/login',(req,res)=>{
-
-  res.redirect('/index.html');
-
-});
-
-//Local - for local database strategy
-app.post('/login', passport.authenticate('local', {
-
-successRedirect: '/dashboard',
-failureRedirect: '/login'
-
-}));
-
-app.get('/dashboard', authenticationMiddleware (), (req,res)=>{
-
-
-//  deserializeUser ... if so - creates a session and returns a session key
-  console.log(req.user);
-  console.log(req.isAuthenticated());
-  res.redirect('/dashboard.html');
-
-	});
-
-
-//verify if the user exists and the password is correct
-passport.use(new LocalStrategy(
-    function (username, password, done) {
-
-        console.log(username);
-        console.log(password);
-
-
-        db.query('SELECT id, password FROM user WHERE username = ?', [username], function (err, result) {
-            if (err) throw err;
-
-            console.log(result[0]);
-
-            //if nothing is returned
-            if (result.length === 0) {
-                done(null, false);
-            }
-
-            console.log(result[0].password.toString());
-            const hash = result[0].password.toString();
-
-            bcrypt.compare(password, hash, function (err, response) {
-                if (response === true) {
-                    return done(null, {
-                        user_id: result[0].id
-                    });
-                } else {
-                    return done(null, false);
-                }
-            });
-
-        });
-
-    }
-));
-
 
 //writing user data in the session
 passport.serializeUser(function (user_id, done) {
@@ -272,11 +121,9 @@ passport.deserializeUser(function (user_id, done) {
     done(null, user_id);
 });
 
-function authenticationMiddleware() {
-    return function (req, res, next) {
-        console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
 
-        if (req.isAuthenticated()) return next();
-        res.redirect('/login');
-    }
-}
+app.use('/login',login);
+app.use('/dashboard',login);
+app.use('/submit', register);
+
+
